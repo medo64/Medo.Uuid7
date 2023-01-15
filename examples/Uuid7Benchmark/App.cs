106575227a3b52e3aba365d21465c45afab9b706
Josip Medved <jmedved@jmedved.com>
2023-01-15 09:14:55 -0800

    Split example and benchmark into a separate project

---
+++ examples/Uuid7Benchmark/App.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using Medo;
+
+namespace Uuid7Generator;
+
+internal static class App {
+
+    public static void Main() {
+        Thread.CurrentThread.Priority = ThreadPriority.Highest;
+
+        var uuids = new Uuid7[5];
+        Uuid7.Fill(uuids);
+
+        foreach (var uuid in uuids) {
+            Console.WriteLine($"UUID: {uuid}");
+            Console.WriteLine($"ID25: {uuid.ToId25String()}");
+            Console.WriteLine($"ID22: {uuid.ToId22String()}");
+            Console.WriteLine();
+        }
+
+        Thread.Sleep(1000);
+
+        {
+            var uuidCount = 0;
+            var sw = Stopwatch.StartNew();
+            while (sw.ElapsedMilliseconds < 5000) {
+                _ = Uuid7.NewUuid7();
+                uuidCount++;
+            }
+            sw.Stop();
+            Console.WriteLine($"Generated {uuidCount:#,##0} UUIDs in {sw.ElapsedMilliseconds:#,##0} millisecond ({uuidCount / sw.ElapsedMilliseconds * 1000:#,##0} per second)");
+        }
+
+        Thread.Sleep(1000);
+
+        {
+            var guidCount = 0;
+            var sw = Stopwatch.StartNew();
+            while (sw.ElapsedMilliseconds < 5000) {
+                _ = Guid.NewGuid();
+                guidCount++;
+            }
+            sw.Stop();
+            Console.WriteLine($"Generated {guidCount:#,##0} GUIDs in {sw.ElapsedMilliseconds:#,##0} millisecond ({guidCount / sw.ElapsedMilliseconds * 1000:#,##0} per second)");
+        }
+
+    }
+}
--- examples/Uuid7Generator/App.cs
+++ examples/Uuid7Generator/App.cs
@@ -1,50 +1,18 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading;
 using Medo;
+
+namespace Uuid7Generator;
 
 internal static class App {
 
     public static void Main() {
-        var uuids = new List<Uuid7>();
         for (var i = 0; i < 5; i++) {
-            uuids.Add(Uuid7.NewUuid7());
-        }
-
-        foreach (var uuid in uuids) {
+            var uuid = Uuid7.NewUuid7();
             Console.WriteLine($"UUID: {uuid}");
             Console.WriteLine($"ID25: {uuid.ToId25String()}");
             Console.WriteLine($"ID22: {uuid.ToId22String()}");
             Console.WriteLine();
         }
-
-        Thread.CurrentThread.Priority = ThreadPriority.Highest;
-        Thread.Sleep(1000);
-
-        {
-            var uuidCount = 0;
-            var sw = Stopwatch.StartNew();
-            while (sw.ElapsedMilliseconds < 5000) {
-                _ = Uuid7.NewUuid7();
-                uuidCount++;
-            }
-            sw.Stop();
-            Console.WriteLine($"Generated {uuidCount:#,##0} UUIDs in {sw.ElapsedMilliseconds:#,##0} millisecond ({uuidCount / sw.ElapsedMilliseconds * 1000:#,##0} per second)");
-        }
-
-        Thread.Sleep(1000);
-
-        {
-            var guidCount = 0;
-            var sw = Stopwatch.StartNew();
-            while (sw.ElapsedMilliseconds < 5000) {
-                _ = Guid.NewGuid();
-                guidCount++;
-            }
-            sw.Stop();
-            Console.WriteLine($"Generated {guidCount:#,##0} GUIDs in {sw.ElapsedMilliseconds:#,##0} millisecond ({guidCount / sw.ElapsedMilliseconds * 1000:#,##0} per second)");
-        }
-
     }
+
 }
