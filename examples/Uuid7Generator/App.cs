c03fcf05d9910a8602cc0e976bef510f91b980ee
Josip Medved <jmedved@jmedved.com>
2023-04-12 23:31:53 -0700

    Timestamps are monotonically increasing even if time goes backward

--- examples/Uuid7Generator/App.cs
+++ examples/Uuid7Generator/App.cs
@@ -6,12 +6,15 @@ namespace Uuid7Generator;
 internal static class App {
 
     public static void Main() {
-        for (var i = 0; i < 5; i++) {
+        while (true) {
             var uuid = Uuid7.NewUuid7();
             Console.WriteLine($"UUID: {uuid}");
             Console.WriteLine($"ID25: {uuid.ToId25String()}");
             Console.WriteLine($"ID22: {uuid.ToId22String()}");
             Console.WriteLine();
+
+            var key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Escape) { break; }
         }
     }
 
