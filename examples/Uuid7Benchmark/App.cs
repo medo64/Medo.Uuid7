1c827c8a1bc13848a221ea810f2b9db7fc0edca6
Josip Medved <jmedved@jmedved.com>
2023-06-14 19:10:39 -0700

    Added multithreaded benchmark

--- examples/Uuid7Benchmark/App.cs
+++ examples/Uuid7Benchmark/App.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Diagnostics;
 using System.Threading;
 using Medo;
 
-namespace Uuid7Generator;
+namespace Uuid7Benchmark;
 
 internal static class App {
 
@@ -20,75 +19,16 @@ internal static class App {
                 Console.WriteLine($"UUID: {uuid}");
                 Console.WriteLine($"ID25: {uuid.ToId25String()}");
                 Console.WriteLine($"ID22: {uuid.ToId22String()}");
-                Console.WriteLine();
             }
-        }
-
-        Thread.Sleep(1000);
-
-        {
-            var uuidCount = 0;
-            var sw = Stopwatch.StartNew();
-            while (sw.ElapsedMilliseconds < 5000) {
-                _ = Uuid7.NewUuid7();
-                uuidCount++;
-            }
-            sw.Stop();
-            Console.WriteLine($"Generated {uuidCount:#,##0} v7 UUIDs in {sw.ElapsedMilliseconds:#,##0} millisecond ({uuidCount / sw.ElapsedMilliseconds * 1000:#,##0} per second)");
-        }
-
-        Thread.Sleep(1000);
-
-        {
-            var uuidCount = 0;
-            var sw = Stopwatch.StartNew();
-            while (sw.ElapsedMilliseconds < 5000) {
-                _ = Uuid7.NewUuid4();
-                uuidCount++;
-            }
-            sw.Stop();
-            Console.WriteLine($"Generated {uuidCount:#,##0} v4 UUIDs in {sw.ElapsedMilliseconds:#,##0} millisecond ({uuidCount / sw.ElapsedMilliseconds * 1000:#,##0} per second)");
-        }
-
-        Thread.Sleep(1000);
-
-        {
-            var guidCount = 0;
-            var sw = Stopwatch.StartNew();
-            while (sw.ElapsedMilliseconds < 5000) {
-                _ = Guid.NewGuid();
-                guidCount++;
-            }
-            sw.Stop();
-            Console.WriteLine($"Generated {guidCount:#,##0} GUIDs in {sw.ElapsedMilliseconds:#,##0} millisecond ({guidCount / sw.ElapsedMilliseconds * 1000:#,##0} per second)");
         }
 
         Console.WriteLine();
-        Thread.Sleep(1000);
+        TestSingleThread.Run();
 
-        {
-            var uuidCount = 0;
-            var sw = Stopwatch.StartNew();
-            while (sw.ElapsedMilliseconds < 5000) {
-                Uuid7.NewUuid7().ToString();
-                uuidCount++;
-            }
-            sw.Stop();
-            Console.WriteLine($"ToString() {uuidCount:#,##0} UUIDs in {sw.ElapsedMilliseconds:#,##0} millisecond ({uuidCount / sw.ElapsedMilliseconds * 1000:#,##0} per second)");
-        }
+        Console.WriteLine();
+        TestMultiThread.Run();
 
-        Thread.Sleep(1000);
-
-        {
-            var uuidCount = 0;
-            var sw = Stopwatch.StartNew();
-            while (sw.ElapsedMilliseconds < 5000) {
-                Guid.NewGuid().ToString();
-                uuidCount++;
-            }
-            sw.Stop();
-            Console.WriteLine($"ToString() {uuidCount:#,##0} GUIDs in {sw.ElapsedMilliseconds:#,##0} millisecond ({uuidCount / sw.ElapsedMilliseconds * 1000:#,##0} per second)");
-        }
-
+        Console.WriteLine();
+        TestToString.Run();
     }
 }
