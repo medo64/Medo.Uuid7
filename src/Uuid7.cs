5c880a6b91d9ca74f39f6df529dc471ee3f0b69d
Josip Medved <jmedved@jmedved.com>
2023-06-24 17:03:38 -0700

    Added comments for IEquatable and IComparable

--- src/Uuid7.cs
+++ src/Uuid7.cs
@@ -665,27 +665,39 @@ public readonly struct Uuid7 : IComparable<Guid>, IComparable<Uuid7>, IEquatable
 
     #endregion Operators
 
-    #region IComparable<Guid>
-
-    /// <inheritdoc/>
-    public int CompareTo(Guid other) {
-        return CompareArrays(Bytes, other.ToByteArray());
-    }
-
-    #endregion IComparable<Guid>
-
     #region IComparable<Uuid>
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Compares this instance to a specified Guid object and returns an indication of their relative values.
+    /// A negative integer if this instance is less than value; a positive integer if this instance is greater than value; or zero if this instance is equal to value.
+    /// </summary>
+    /// <param name="other">An object to compare to this instance.</param>
     public int CompareTo(Uuid7 other) {
         return CompareArrays(Bytes, other.Bytes);
     }
 
     #endregion IComparable<Uuid>
 
+    #region IComparable<Guid>
+
+    /// <summary>
+    /// Compares this instance to a specified Guid object and returns an indication of their relative values.
+    /// A negative integer if this instance is less than value; a positive integer if this instance is greater than value; or zero if this instance is equal to value.
+    /// GUID and UUID are compared for their binary value.
+    /// </summary>
+    /// <param name="other">An object to compare to this instance.</param>
+    public int CompareTo(Guid other) {
+        return CompareArrays(Bytes, other.ToByteArray());
+    }
+
+    #endregion IComparable<Guid>
+
     #region IEquatable<Uuid>
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns a value that indicates whether this instance is equal to a specified object.
+    /// </summary>
+    /// <param name="other">An object to compare to this instance.</param>
     public bool Equals(Uuid7 other) {
         return CompareArrays(Bytes, other.Bytes) == 0;
     }
@@ -694,7 +706,11 @@ public readonly struct Uuid7 : IComparable<Guid>, IComparable<Uuid7>, IEquatable
 
     #region IEquatable<Guid>
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns a value that indicates whether this instance is equal to a specified object.
+    /// Objects are considered equal if they have the same binary representation.
+    /// </summary>
+    /// <param name="other">An object to compare to this instance.</param>
     public bool Equals(Guid other) {
         return CompareArrays(Bytes, other.ToByteArray()) == 0;
     }
