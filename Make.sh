11f9cadf2ca36c8e4d9c03f8b42d61bfdafbdc05
Josip Medved <jmedved@jmedved.com>
2023-07-02 10:34:52 -0700

    Updated make file to handle multiple projects

--- Make.sh
+++ Make.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 BASE_DIRECTORY="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-PROJECT_FILE="$BASE_DIRECTORY/src/Medo.Uuid7.csproj"
-TEST_PROJECT_FILE="$BASE_DIRECTORY/tests/Medo.Uuid7.Tests.csproj"
 PACKAGE_CONTENT_FILES="Makefile Make.sh CHANGELOG.md CONTRIBUTING.md ICON.png LICENSE.md README.md .editorconfig"
 PACKAGE_CONTENT_DIRECTORIES="src/ tests/ examples/"
 
@@ -13,6 +11,7 @@ if [ -t 1 ]; then
     ANSI_RED="`[ $(tput colors) -ge 16 ] && tput setaf 9 || tput setaf 1 bold`"
     ANSI_GREEN="`[ $(tput colors) -ge 16 ] && tput setaf 10 || tput setaf 2 bold`"
     ANSI_YELLOW="`[ $(tput colors) -ge 16 ] && tput setaf 11 || tput setaf 3 bold`"
+    ANSI_MAGENTA="`[ $(tput colors) -ge 16 ] && tput setaf 13 || tput setaf 5 bold`"
     ANSI_CYAN="`[ $(tput colors) -ge 16 ] && tput setaf 14 || tput setaf 6 bold`"
     ANSI_WHITE="`[ $(tput colors) -ge 16 ] && tput setaf 15 || tput setaf 7 bold`"
 fi
@@ -52,10 +51,16 @@ if ! command -v dotnet >/dev/null; then
 fi
 echo ".NET `dotnet --version`"
 
-if [[ "$PROJECT_FILE" == "" ]]; then
+FOUND_PROJECTS=0
+for PROJECT_FILE in $(find $BASE_DIRECTORY/src -name "*.csproj" | sort); do
+    FOUND_PROJECTS=1
+    break
+done
+if [[ $FOUND_PROJECTS -eq 0 ]]; then
     echo "${ANSI_RED}No project file found!${ANSI_RESET}" >&2
     exit 1
 fi
+
 
 PACKAGE_ID=`cat "$PROJECT_FILE" | grep "<PackageId>" | sed 's^</\?PackageId>^^g' | xargs`
 PACKAGE_VERSION=`cat "$PROJECT_FILE" | grep "<Version>" | sed 's^</\?Version>^^g' | xargs`
@@ -105,15 +110,25 @@ function debug() {
     echo
     mkdir -p "$BASE_DIRECTORY/bin/"
     mkdir -p "$BASE_DIRECTORY/build/debug/"
-    dotnet build "$PROJECT_FILE" \
-                 --configuration "Debug" \
-                 --verbosity "minimal" \
-                 || return 1
+
     ATLEAST_ONE_COPY=0
-    for FRAMEWORK in $PACKAGE_FRAMEWORKS; do
-        cp -r "$BASE_DIRECTORY/src/bin/Debug/$FRAMEWORK/" "$BASE_DIRECTORY/bin/" 2>/dev/null
-        if [[ $? -eq 0 ]]; then ATLEAST_ONE_COPY=1; fi
+    for PROJECT_FILE in $(find $BASE_DIRECTORY/src -name "*.csproj" | sort); do
+        echo ; echo "${ANSI_MAGENTA}$PROJECT_FILE${ANSI_RESET}"
+
+        BASE_NAME=$(basename "$PROJECT_FILE" | rev | cut -d. -f2- | rev)
+        mkdir -p "$BASE_DIRECTORY/bin/$BASE_NAME/"
+        mkdir -p "$BASE_DIRECTORY/build/debug/$BASE_NAME/"
+
+        dotnet build "$PROJECT_FILE" \
+                    --configuration "Debug" \
+                    --verbosity "minimal" \
+                    || return 1
+        for FRAMEWORK in $PACKAGE_FRAMEWORKS; do
+            cp -r "$BASE_DIRECTORY/src/bin/Debug/$FRAMEWORK/" "$BASE_DIRECTORY/bin/$BASE_NAME/" 2>/dev/null
+            if [[ $? -eq 0 ]]; then ATLEAST_ONE_COPY=1; fi
+        done
     done
+
     if [[ "$ATLEAST_ONE_COPY" -eq 0 ]]; then return 1; fi
     echo
     echo "${ANSI_GREEN}Output in ${ANSI_CYAN}bin/${ANSI_RESET}"
@@ -126,15 +141,25 @@ function release() {
     fi
     mkdir -p "$BASE_DIRECTORY/bin/"
     mkdir -p "$BASE_DIRECTORY/build/release/"
-    dotnet build "$PROJECT_FILE" \
-                 --configuration "Release" \
-                 --verbosity "minimal" \
-                 || return 1
+
     ATLEAST_ONE_COPY=0
-    for FRAMEWORK in $PACKAGE_FRAMEWORKS; do
-        cp -r "$BASE_DIRECTORY/src/bin/Release/$FRAMEWORK/" "$BASE_DIRECTORY/bin/" 2>/dev/null
-        if [[ $? -eq 0 ]]; then ATLEAST_ONE_COPY=1; fi
+    for PROJECT_FILE in $(find $BASE_DIRECTORY/src -name "*.csproj" | sort); do
+        echo ; echo "${ANSI_MAGENTA}$PROJECT_FILE${ANSI_RESET}"
+
+        BASE_NAME=$(basename "$PROJECT_FILE" | rev | cut -d. -f2- | rev)
+        mkdir -p "$BASE_DIRECTORY/bin/$BASE_NAME/"
+        mkdir -p "$BASE_DIRECTORY/build/debug/$BASE_NAME/"
+
+        dotnet build "$PROJECT_FILE" \
+                    --configuration "Release" \
+                    --verbosity "minimal" \
+                    || return 1
+        for FRAMEWORK in $PACKAGE_FRAMEWORKS; do
+            cp -r "$BASE_DIRECTORY/src/bin/Release/$FRAMEWORK/" "$BASE_DIRECTORY/bin/$BASE_NAME" 2>/dev/null
+            if [[ $? -eq 0 ]]; then ATLEAST_ONE_COPY=1; fi
+        done
     done
+
     if [[ "$ATLEAST_ONE_COPY" -eq 0 ]]; then return 1; fi
     echo
     echo "${ANSI_GREEN}Output in ${ANSI_CYAN}bin/${ANSI_RESET}"
@@ -142,15 +167,29 @@ function release() {
 
 function test() {
     echo
-    if [[ "$TEST_PROJECT_FILE" == "" ]]; then
+    FOUND_PROJECTS=0
+    for TEST_PROJECT_FILE in $(find $BASE_DIRECTORY/tests -name "*.csproj" | sort); do
+        FOUND_TEST_PROJECTS=1
+        break
+    done
+    if [[ $FOUND_TEST_PROJECTS -eq 0 ]]; then
         echo "${ANSI_RED}No test project file found!${ANSI_RESET}" >&2
         exit 1
     fi
+
     mkdir -p "$BASE_DIRECTORY/build/test/"
-    dotnet test "$TEST_PROJECT_FILE" \
-                --configuration "Debug" \
-                --verbosity "minimal" \
-                || return 1
+    for TEST_PROJECT_FILE in $(find $BASE_DIRECTORY/tests -name "*.csproj" | sort); do
+        echo ; echo "${ANSI_MAGENTA}$TEST_PROJECT_FILE${ANSI_RESET}"
+
+        BASE_NAME=$(basename "$TEST_PROJECT_FILE" | rev | cut -d. -f2- | rev)
+        mkdir -p "$BASE_DIRECTORY/build/test/$BASE_NAME/"
+
+        dotnet test "$TEST_PROJECT_FILE" \
+                    --configuration "Debug" \
+                    --verbosity "minimal" \
+                    || return 1
+    done
+
     echo
     echo "${ANSI_GREEN}Testing completed${ANSI_RESET}"
 }
@@ -158,18 +197,31 @@ function test() {
 function package() {
     echo
     mkdir -p "$BASE_DIRECTORY/build/package/"
-    dotnet pack "$PROJECT_FILE" \
-                --configuration "Release" \
-                --force \
-                --include-source \
-                --output "$BASE_DIRECTORY/build/package/" \
-                --verbosity "minimal" \
-                || return 1
-    mkdir -p "$BASE_DIRECTORY/dist/"
-    cp "$BASE_DIRECTORY/build/package/$PACKAGE_ID.$PACKAGE_VERSION.nupkg" "$BASE_DIRECTORY/dist/" || return 1
-    cp "$BASE_DIRECTORY/build/package/$PACKAGE_ID.$PACKAGE_VERSION.snupkg" "$BASE_DIRECTORY/dist/" || return 1
-    echo
-    echo "${ANSI_GREEN}Output at ${ANSI_CYAN}dist/$PACKAGE_ID-$PACKAGE_VERSION.nupkg${ANSI_RESET}"
+
+    for PROJECT_FILE in $(find $BASE_DIRECTORY/src -name "*.csproj" | sort); do
+        echo ; echo "${ANSI_MAGENTA}$PROJECT_FILE${ANSI_RESET}"
+
+        BASE_NAME=$(basename "$PROJECT_FILE" | rev | cut -d. -f2- | rev)
+        mkdir -p "$BASE_DIRECTORY/build/package/$BASE_NAME"
+
+        PACKAGE_ID=`cat "$PROJECT_FILE" | grep "<PackageId>" | sed 's^</\?PackageId>^^g' | xargs`
+        PACKAGE_VERSION=`cat "$PROJECT_FILE" | grep "<Version>" | sed 's^</\?Version>^^g' | xargs`
+        PACKAGE_FRAMEWORKS=`cat "$PROJECT_FILE" | grep "<TargetFramework" | sed 's^</\?TargetFrameworks\?>^^g' | tr ';' ' ' | xargs`
+
+        dotnet pack "$PROJECT_FILE" \
+                    --configuration "Release" \
+                    --force \
+                    --include-source \
+                    --output "$BASE_DIRECTORY/build/package/$BASE_NAME" \
+                    --verbosity "minimal" \
+                    || return 1
+
+        mkdir -p "$BASE_DIRECTORY/dist/$BASE_NAME"
+        cp "$BASE_DIRECTORY/build/package/$BASE_NAME/$PACKAGE_ID.$PACKAGE_VERSION.nupkg" "$BASE_DIRECTORY/dist/$BASE_NAME/" || return 1
+        cp "$BASE_DIRECTORY/build/package/$BASE_NAME/$PACKAGE_ID.$PACKAGE_VERSION.snupkg" "$BASE_DIRECTORY/dist/$BASE_NAME/" || return 1
+        echo
+        echo "${ANSI_GREEN}Output at ${ANSI_CYAN}dist/$BASE_NAME/$PACKAGE_ID-$PACKAGE_VERSION.nupkg${ANSI_RESET}"
+    done
     return 0
 }
 
@@ -179,16 +231,28 @@ function nuget() {  # (api_key)
         echo "${ANSI_RED}No key in .nuget.key!${ANSI_RESET}" >&2
         return 1;
     fi
-    if [[ "$PACKAGE_VERSION" == "0.0.0" ]]; then
-        echo "${ANSI_RED}No version in project file!${ANSI_RESET}" >&2
-        return 1;
-    fi
-    dotnet nuget push "$BASE_DIRECTORY/dist/$PACKAGE_ID.$PACKAGE_VERSION.nupkg" \
-                      --source "https://api.nuget.org/v3/index.json" \
-                      --api-key "$API_KEY" \
-                      --symbol-api-key "$API_KEY" \
-                      || return 1
-    echo "${ANSI_GREEN}Sent to ${ANSI_CYAN}dist/$PACKAGE_ID-$PACKAGE_VERSION.nupkg${ANSI_RESET}"
+
+    for PROJECT_FILE in $(find $BASE_DIRECTORY/src -name "*.csproj" | sort); do
+        PACKAGE_VERSION=`cat "$PROJECT_FILE" | grep "<Version>" | sed 's^</\?Version>^^g' | xargs`
+        if [[ "$PACKAGE_VERSION" == "0.0.0" ]]; then
+            echo "${ANSI_RED}No version in project file!${ANSI_RESET}" >&2
+            #return 1;
+        fi
+    done
+
+    for PROJECT_FILE in $(find $BASE_DIRECTORY/src -name "*.csproj" | sort); do
+        echo ; echo "${ANSI_MAGENTA}$PROJECT_FILE${ANSI_RESET}"
+
+        PACKAGE_ID=`cat "$PROJECT_FILE" | grep "<PackageId>" | sed 's^</\?PackageId>^^g' | xargs`
+        PACKAGE_VERSION=`cat "$PROJECT_FILE" | grep "<Version>" | sed 's^</\?Version>^^g' | xargs`
+
+        dotnet nuget push "$BASE_DIRECTORY/dist/$BASE_NAME/$PACKAGE_ID.$PACKAGE_VERSION.nupkg" \
+                          --source "https://api.nuget.org/v3/index.json" \
+                          --api-key "$API_KEY" \
+                          --symbol-api-key "$API_KEY" \
+                          || return 1
+        echo "${ANSI_GREEN}Sent ${ANSI_CYAN}dist/$BASE_NAME/$PACKAGE_ID-$PACKAGE_VERSION.nupkg${ANSI_RESET}"
+    done
     return 0
 }
 
