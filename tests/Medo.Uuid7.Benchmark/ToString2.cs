87b1b6db17e9a067e7d1b4573f3ab34bc5e05e35
Josip Medved <jmedved@jmedved.com>
2023-07-14 22:29:20 -0700

    Added benchmarks for ToId22 and ToId25

--- tests/Medo.Uuid7.Benchmark/ToString2.cs
+++ tests/Medo.Uuid7.Benchmark/ToString2.cs
@@ -7,9 +7,13 @@ using Medo;
 
 public class ToString2 {
 
+    private Guid ExampleGuid =  Guid.NewGuid();
     private Uuid7 ExampleUuid7 =  Uuid7.NewUuid7();
     private Uuid7 ExampleUuid4 =  Uuid7.NewUuid4();
 
+
+    [Benchmark(Baseline = true)]
+    public string ToStringGuid() => ExampleGuid.ToString("");
 
     [Benchmark]
     public string ToStringUuid72() => ExampleUuid7.ToString("2");
--- tests/Medo.Uuid7.Benchmark/ToString5.cs
+++ tests/Medo.Uuid7.Benchmark/ToString5.cs
@@ -7,9 +7,13 @@ using Medo;
 
 public class ToString5 {
 
+    private Guid ExampleGuid =  Guid.NewGuid();
     private Uuid7 ExampleUuid7 =  Uuid7.NewUuid7();
     private Uuid7 ExampleUuid4 =  Uuid7.NewUuid4();
 
+
+    [Benchmark(Baseline = true)]
+    public string ToStringGuid() => ExampleGuid.ToString("");
 
     [Benchmark]
     public string ToStringUuid75() => ExampleUuid7.ToString("5");
