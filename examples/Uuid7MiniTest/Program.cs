646bc61fe638cb3cd35379f9baa54a6f8ffdd83a
Josip Medved <jmedved@jmedved.com>
2023-08-19 21:21:23 -0700

    Added mini test to fill data for uuid6/uuid6-ietf-draft

---
+++ examples/Uuid7MiniTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Medo;
+using System.Diagnostics;
+
+{
+    var stopwatch = Stopwatch.StartNew();
+    const int guidCount = 100_000_000;
+    for (var i = 0; i < guidCount; i++)
+        Uuid7.NewUuid4();
+    stopwatch.Stop();
+    Console.WriteLine($"{guidCount:n0} v4 UUIDs in {stopwatch.ElapsedMilliseconds:n0}ms; {((double)guidCount / stopwatch.ElapsedMilliseconds):f1}/ms");
+}
+
+{
+    var stopwatch = Stopwatch.StartNew();
+    const int guidCount = 100_000_000;
+    for (var i = 0; i < guidCount; i++)
+        Uuid7.NewUuid7();
+    stopwatch.Stop();
+    Console.WriteLine($"{guidCount:n0} v7 UUIDs in {stopwatch.ElapsedMilliseconds:n0}ms; {((double)guidCount / stopwatch.ElapsedMilliseconds):f1}/ms");
+}
