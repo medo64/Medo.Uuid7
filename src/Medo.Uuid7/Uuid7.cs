559a87485d22866156ee22672c46d10da2e640fd
Josip Medved <jmedved@jmedved.com>
2024-06-29 11:27:07 -0700

    Fixed MS SQL UniqueID creation

--- src/Medo.Uuid7/Uuid7.cs
+++ src/Medo.Uuid7/Uuid7.cs
@@ -189,9 +189,8 @@ public readonly struct Uuid7
     public static Guid NewGuidMsSql() {
         var bytes = new byte[16];
         lock (NonThreadedSyncRoot) {
-            FillBytes7(ref bytes, DateTime.UtcNow.Ticks, ref NonThreadedLastMillisecond, ref NonThreadedMillisecondCounter, ref NonThreadedMonotonicCounter);  // DateTime is a smidgen faster than DateTimeOffset
+            FillBytes7MsSql(ref bytes, DateTime.UtcNow.Ticks, ref NonThreadedLastMillisecond, ref NonThreadedMillisecondCounter, ref NonThreadedMonotonicCounter);  // DateTime is a smidgen faster than DateTimeOffset
         }
-        AdjustGuidEndianess(ref bytes);
         return new Guid(bytes);
     }
 
@@ -252,6 +251,18 @@ public readonly struct Uuid7
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
     private static void FillBytes7(ref byte[] bytes, long ticks, ref long lastMillisecond, ref long millisecondCounter, ref uint monotonicCounter) {
+        //   0                   1                   2                   3
+        //   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+        //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        //  |                       unix_ts_ms[0:31]                        |
+        //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        //  |       unix_ts_ms[32:47]       |  ver  |     counter[0:11]     |
+        //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        //  |var|      counter[12:25]       |            random             |
+        //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        //  |                            random                             |
+        //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
         var millisecond = unchecked(ticks / TicksPerMillisecond);
         var msCounter = millisecondCounter;
 
@@ -283,6 +294,67 @@ public readonly struct Uuid7
             monoCounter = (uint)(((bytes[6] & 0x07) << 22) | (bytes[7] << 14) | ((bytes[8] & 0x3F) << 8) | bytes[9]);  // to use as monotonic random for future calls; total of 26 bits but only 25 are used initially with upper 1 bit reserved for rollover guard
         } else {
             GetRandomBytes(ref bytes, 9, 7);
+            monoCounter = unchecked(monotonicCounter + ((uint)bytes[9] >> 4) + 1);  // 4 bit random increment will reduce overall counter space by 3 bits on average (to 2^22 combinations)
+            bytes[7] = (byte)(monoCounter >> 14);    // bits 14:21 of monotonics counter
+            bytes[9] = (byte)(monoCounter);          // bits 0:7 of monotonics counter
+        }
+        monotonicCounter = monoCounter;
+
+        //Fixup
+        bytes[6] = (byte)(0x70 | ((monoCounter >> 22) & 0x0F));  // set 4-bit version + bits 22:25 of monotonics counter
+        bytes[8] = (byte)(0x80 | ((monoCounter >> 8) & 0x3F));   // set 2-bit variant + bits 8:13 of monotonics counter
+    }
+
+#if NET6_0_OR_GREATER
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+#else
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+    private static void FillBytes7MsSql(ref byte[] bytes, long ticks, ref long lastMillisecond, ref long millisecondCounter, ref uint monotonicCounter) {
+        //   0                   1                   2                   3
+        //   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+        //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        //  |                            random                             |
+        //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        //  |            random             |  ver  |     counter[14:25]    |
+        //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        //  |var|       counter[0:13]       |       unix_ts_ms[0:15]        |
+        //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        //  |                       unix_ts_ms[16:47]                       |
+        //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+
+        var millisecond = unchecked(ticks / TicksPerMillisecond);
+        var msCounter = millisecondCounter;
+
+        var newStep = (millisecond != lastMillisecond);
+        if (newStep) {  // we need to switch millisecond (i.e. counter)
+            lastMillisecond = millisecond;
+            long ms;
+            ms = unchecked(millisecond - UnixEpochMilliseconds);
+            if (msCounter < ms) {  // normal time progression
+                msCounter = ms;
+            } else {  // time went backward, just increase counter
+                unchecked { msCounter++; }
+            }
+            millisecondCounter = msCounter;
+        }
+
+        // Timestamp
+        bytes[10] = (byte)(msCounter >> 40);
+        bytes[11] = (byte)(msCounter >> 32);
+        bytes[12] = (byte)(msCounter >> 24);
+        bytes[13] = (byte)(msCounter >> 16);
+        bytes[14] = (byte)(msCounter >> 8);
+        bytes[15] = (byte)msCounter;
+
+        // Randomness
+        uint monoCounter;
+        if (newStep) {
+            GetRandomBytes(ref bytes, 0, 10);
+            monoCounter = (uint)(((bytes[6] & 0x07) << 22) | (bytes[7] << 14) | ((bytes[8] & 0x3F) << 8) | bytes[9]);  // to use as monotonic random for future calls; total of 26 bits but only 25 are used initially with upper 1 bit reserved for rollover guard
+        } else {
+            GetRandomBytes(ref bytes, 0, 7);
             monoCounter = unchecked(monotonicCounter + ((uint)bytes[9] >> 4) + 1);  // 4 bit random increment will reduce overall counter space by 3 bits on average (to 2^22 combinations)
             bytes[7] = (byte)(monoCounter >> 14);    // bits 14:21 of monotonics counter
             bytes[9] = (byte)(monoCounter);          // bits 0:7 of monotonics counter
@@ -337,7 +409,6 @@ public readonly struct Uuid7
         }
     }
 
-
     #endregion Endianess
 
 
@@ -355,11 +426,9 @@ public readonly struct Uuid7
     /// This should be used only when using MS SQL Server and not any other. If
     /// you are using Uuid7 in mixed database environment, use ToGuid() instead.
     /// </summary>
+    [Obsolete("Use NewGuidMsSql instead")]
     public Guid ToGuidMsSql() {
-        var bytes = new byte[16];
-        Buffer.BlockCopy(Bytes, 0, bytes, 0, 16);
-        AdjustGuidEndianess(ref bytes);
-        return new Guid(bytes);
+        throw new NotSupportedException("Conversion is no longer supported.");
     }
 
 
