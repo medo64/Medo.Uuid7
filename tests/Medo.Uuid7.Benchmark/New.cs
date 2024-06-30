9b604ca1001fa32630baedfcad6983aa46296c07
Josip Medved <jmedved@jmedved.com>
2024-06-29 22:40:15 -0700

    Added Guid benchmarks

--- tests/Medo.Uuid7.Benchmark/New.cs
+++ tests/Medo.Uuid7.Benchmark/New.cs
@@ -8,15 +8,24 @@ using Medo;
 public class New {
 
     [Benchmark(Baseline = true)]
-    public Guid NewGuid() => Guid.NewGuid();
+    public Guid NewSystemGuid() => Guid.NewGuid();
 
     [Benchmark]
     public void NewUuid7() => Uuid7.NewUuid7();
 
     [Benchmark]
-    public void NewMsSqlUniqueIdentifier() => Uuid7.NewMsSqlUniqueIdentifier();
+    public void NewUuid4() => Uuid7.NewUuid4();
 
     [Benchmark]
-    public void NewUuid4() => Uuid7.NewUuid4();
+    public void NewGuid() => Uuid7.NewGuid();
+
+    [Benchmark]
+    public void NewGuidNonMatched() => Uuid7.NewGuid(matchGuidEndianness: false);
+
+    [Benchmark]
+    public void NewGuidMatched() => Uuid7.NewGuid(matchGuidEndianness: true);
+
+    [Benchmark]
+    public void NewMsSqlUniqueIdentifier() => Uuid7.NewMsSqlUniqueIdentifier();
 
 }
